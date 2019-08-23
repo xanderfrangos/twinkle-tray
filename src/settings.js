6e7f863022ddbb9e81a9680c82ed17408d1d8b4f
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2019-08-22 22:38:07 -0400

    Clear

--- src/settings.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SettingsWindow from "./Components/SettingsWindow";
-
-ReactDOM.render(<SettingsWindow theme={window.settings.theme} />, document.getElementById("settings"));

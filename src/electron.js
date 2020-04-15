a140c363f12ac97021216f2cb37525758227e9f1
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2020-04-15 14:43:20 -0400

    Fixed incorrect primary display detection

--- src/electron.js
+++ src/electron.js
@@ -1066,7 +1066,7 @@ function createPanel(toggleOnLoad = false) {
 function repositionPanel() {
   let displays = screen.getAllDisplays()
   let primaryDisplay = displays.find((display) => {
-    return display.bounds.x == 0 || display.bounds.y == 0
+    return display.bounds.x == 0 && display.bounds.y == 0
   })
 
   const taskbar = taskbarPosition()

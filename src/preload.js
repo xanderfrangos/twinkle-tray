444c7e00c6ea1d6297f9334e0928a25e36f48181
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2020-04-16 21:17:17 -0400

    Moved "turn off displays" to electron.js

--- src/preload.js
+++ src/preload.js
@@ -79,7 +79,6 @@ function panelAnimationDone() {
 function turnOffDisplays() {
     setPanelVisibility(false)
     ipc.send('sleep-displays')
-    exec(`powershell.exe (Add-Type '[DllImport(\\"user32.dll\\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)`)
 }
 
 function installUpdate() {

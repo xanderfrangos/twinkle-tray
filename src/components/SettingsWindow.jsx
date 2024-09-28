64f6d516ffc9b51c2edaf0489b2f4f0c4e6f1b2c
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2024-09-28 16:16:07 -0400

    Add debug option for useWmDisplayChangeEvent

--- src/components/SettingsWindow.jsx
+++ src/components/SettingsWindow.jsx
@@ -1425,6 +1425,7 @@ export default class SettingsWindow extends PureComponent {
                                     <SettingsOption title="Disable Auto Refresh" description="Prevent last known brightness from read after certain hardware/user events." input={this.renderToggle("disableAutoRefresh")} />
                                     <SettingsOption title="Use Win32 hardware events" input={this.renderToggle("useWin32Event")} />
                                     <SettingsOption title="Use Electron hardware events" input={this.renderToggle("useElectronEvents")} />
+                                    <SettingsOption title="Use WM_DISPLAYCHANGE events" input={this.renderToggle("useWmDisplayChangeEvent")} />
                                     <SettingsOption title="Show console window (requires restart)" input={this.renderToggle("showConsole")} />
                                     <SettingsOption title="Use Taskbar Registry" input={this.renderToggle("useTaskbarRegistry")} />
                                     <SettingsOption title="Disable Mouse Events (requires restart)" input={this.renderToggle("disableMouseEvents")} />

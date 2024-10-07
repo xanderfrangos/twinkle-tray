45b1eba635004ee0afa63fcd60e6c0c66fca7869
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2024-10-06 20:37:55 -0400

    Add SetWindowAttribute bindings

--- src/modules/tt-windows-utils/index.js
+++ src/modules/tt-windows-utils/index.js
@@ -3,6 +3,7 @@ const WindowUtils = require("bindings")("windows_window_utils");
 const PowerEvents = require("bindings")("windows_power_events");
 const MediaStatus = require("bindings")("windows_media_status");
 const AppStartup = require("bindings")("windows_app_startup");
+const WindowMaterial = require("bindings")("windows_window_material");
 
 module.exports = {
     WindowUtils: {
@@ -26,5 +27,20 @@ module.exports = {
     AppStartup: {
         enable: AppStartup.enable,
         disable: AppStartup.disable
+    },
+    WindowMaterial: {
+        setWindowMaterial: (hwnd, materialType = 1, cornersType = 2, darkModeSupported = true) => {
+            WindowMaterial.setWindowMaterial(hwnd, materialType, cornersType, (darkModeSupported ? 1 : 0))
+        },
+        setWindowAttribute: WindowMaterial.setWindowAttribute,
+        setWindowCorners: (hwnd, cornerType = 0) => {
+            WindowMaterial.setWindowAttribute(hwnd, 33, cornerType)
+        },
+        setDarkModeSupported: (hwnd, enabled = 1) => {
+            WindowMaterial.setWindowAttribute(hwnd, 20, enabled)
+        },
+        setTransitionSupported: (hwnd, enabled = 1) => {
+            WindowMaterial.setWindowAttribute(hwnd, 3, enabled)
+        }
     }
 }

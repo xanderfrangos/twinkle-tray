7768841ca782950d43be9e52071d2fc7e1548663
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2026-08-12 22:28:40 -0400

    Remove "Refreshing" text notices

--- src/components/DDCCISliders.jsx
+++ src/components/DDCCISliders.jsx
@@ -109,8 +109,6 @@ export default function DDCCISliders(props) {
 
     return (
         <>
-            {monitor.featuresPending ? (<div className="feature-row feature-pending">Detecting display features…</div>) : null}
-            {monitor.featuresRefreshing ? (<div className="feature-row feature-pending">Refreshing display features…</div>) : null}
             {extraHTML}
         </>
     )
